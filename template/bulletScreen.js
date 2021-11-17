873454aad36591b7f7fba6d19577eb9ec86c8249
hezijun <zijunhe@digitalgd.com.cn>
2021-11-17 17:54:42 +0800

    add

--- template/bulletScreen.js
+++ template/bulletScreen.js
@@ -10,8 +10,7 @@ const script = document.createElement('script')
 script.onload = resolve
 script.onerror = reject
 script.async = true
-script.src = 'https://cdn.jsdelivr.net/npm/'
-    + '@fingerprintjs/fingerprintjs@3/dist/fp.min.js'
+script.src = './fp.min.js'
 document.head.appendChild(script)
 })
 .then(() => FingerprintJS.load())
